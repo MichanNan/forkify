70f8de97efcd9c6274dc7d31f80e27d873069718
Nan <caonanla@gmail.com>
2023-11-07 11:46:48 +0100

    add error handling

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,14 +6,6 @@ import 'regenerator-runtime/runtime';
 
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
@@ -31,12 +23,12 @@ const controllRecipes = async function () {
     //rendering recipe
     recipeView.render(model.state.recipe);
   } catch (error) {
-    alert(error);
+    recipeView.renderError();
   }
 };
 
-// window.addEventListener('hashchange', showRecipe);
-// window.addEventListener('load', showRecipe);
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controllRecipes)
-);
+const init = function () {
+  recipeView.addHandlerRender(controllRecipes);
+};
+
+init();
